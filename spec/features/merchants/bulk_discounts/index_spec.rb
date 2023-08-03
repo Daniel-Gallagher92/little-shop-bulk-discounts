9d456fb96e4247c258ba323a4dd61a2277752e0d
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-08-03 17:44:12 -0600

    US_1 tests and functionality complete

--- spec/features/merchants/bulk_discounts/index_spec.rb
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -8,9 +8,9 @@ RSpec.describe 'Merchant Bulk Discounts Index Page' do
 
       bulk_discount_1 = merchant_1.bulk_discounts.create!(percentage_discount: 5, quantity_threshold: 10)
       bulk_discount_2 = merchant_1.bulk_discounts.create!(percentage_discount: 10, quantity_threshold: 15)
-      bulk_discount_2 = merchant_1.bulk_discounts.create!(percentage_discount: 20, quantity_threshold: 20)
-      bulk_discount_2 = merchant_1.bulk_discounts.create!(percentage_discount: 25, quantity_threshold: 30)
-      bulk_discount_2 = merchant_1.bulk_discounts.create!(percentage_discount: 30, quantity_threshold: 50)
+      bulk_discount_3 = merchant_1.bulk_discounts.create!(percentage_discount: 20, quantity_threshold: 20)
+      bulk_discount_4 = merchant_1.bulk_discounts.create!(percentage_discount: 25, quantity_threshold: 30)
+      bulk_discount_5 = merchant_1.bulk_discounts.create!(percentage_discount: 30, quantity_threshold: 50)
 
       customer_1 = create(:customer)
 
@@ -18,11 +18,19 @@ RSpec.describe 'Merchant Bulk Discounts Index Page' do
       items_2 = create_list(:item, 15, unit_price: 50)
 
       visit merchant_bulk_discounts_path(merchant_1)
-
+      save_and_open_page
       expect(page).to have_content("Bulk Discounts")
       expect(page).to have_link("#{bulk_discount_1.id}")
       expect(page).to have_content("Quantity Threshold: #{bulk_discount_1.quantity_threshold}")
       expect(page).to have_content("Percentage Discount: #{bulk_discount_1.percentage_discount}% off!")
+
+      expect(page).to have_link("#{bulk_discount_2.id}")
+      expect(page).to have_content("Quantity Threshold: #{bulk_discount_2.quantity_threshold}")
+      expect(page).to have_content("Percentage Discount: #{bulk_discount_2.percentage_discount}% off!")
+
+      expect(page).to have_link("#{bulk_discount_3.id}")
+      expect(page).to have_content("Quantity Threshold: #{bulk_discount_3.quantity_threshold}")
+      expect(page).to have_content("Percentage Discount: #{bulk_discount_3.percentage_discount}% off!")
     end
   end
 end
