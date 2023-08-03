df63fbb676cba74903d93aba0cd9856cbaf16fa3
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-08-03 16:54:50 -0600

    associations

--- spec/features/merchants/bulk_discounts/index_spec.rb
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'Merchant Bulk Discounts Index Page' do
-  describe 
+  describe "US_1 (pt 2)" do 
+    it 'displays all bulk discounts with percentage of discount and quantity threshold' do
+      merchant_1 = create(:merchant)
+      merchant_2 = create(:merchant)
+
+      bulk_discount_1 = merchant_1.bulk_discounts.create!(percentage_discount: 5, quantity_threshold: 10)
+      bulk_discount_2 = merchant_1.bulk_discounts.create!(percentage_discount: 10, quantity_threshold: 15)
+      bulk_discount_2 = merchant_1.bulk_discounts.create!(percentage_discount: 20, quantity_threshold: 20)
+      bulk_discount_2 = merchant_1.bulk_discounts.create!(percentage_discount: 25, quantity_threshold: 30)
+      bulk_discount_2 = merchant_1.bulk_discounts.create!(percentage_discount: 30, quantity_threshold: 50)
+
+      customer_1 = create(:customer)
+
+      items_1 = create_list(:item, 10, unit_price: 50)
+      items_2 = create_list(:item, 15, unit_price: 50)
+
+      visit merchant_bulk_discounts_path(merchant_1)
+
+      expect(page).to have_content("Bulk Discounts")
+      expect(page).to have_link("#{bulk_discount_1.id}")
+      expect(page).to have_content("Quantity Threshold: #{bulk_discount_1.quantity_threshold}")
+      expect(page).to have_content("Percentage Discount: #{bulk_discount_1.percentage_discount}% off!")
+    end
+  end
 end
